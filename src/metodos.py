e17b4d38d548de85b5c0cb3f5c5422d3c3336b00
FranJMD0508 <fmejia22@ujap.edu.ve>
2024-11-15 22:13:04 -0400

    Añadido el método de Trazador Cúbico y su archivo de prueba

--- src/metodos.py
+++ src/metodos.py
@@ -1,3 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sp
+
+def Trazador_Cubico(x_data, y_data, x_vals):
+    trazado = sp.interpolate.CubicSpline(x_data, y_data)
+    plt.plot(x_data, y_data, 'o', label='Datos')
+    plt.plot(x_vals, trazado(x_vals), label='Trazador Cúbico')
+    plt.title('Método de Trazador Cúbico Sujeto')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.legend()
+    plt.show()
+    return trazado(x_vals)
--- test/testCubico.py
+++ test/testCubico.py
@@ -1,3 +1,16 @@
 import numpy as np
 from pandas import read_csv
 import unittest
+from src.metodos import Trazador_Cubico 
+
+class TestCubico(unittest.TestCase):
+    def test_trazador_cubico(self):
+        datos = read_csv('trazado_cubico.csv')
+        x_data = datos['x'].values
+        y_data = datos['y'].values
+        valores = datos["x_vals"].values
+        x_vals = np.linspace(valores[0], valores[1], int(valores[2]))
+        resultado = Trazador_Cubico(x_data, y_data, x_vals)
+        np.testing.assert_allclose(resultado[0], 0.5, atol=0.1)
+
+unittest.main()
